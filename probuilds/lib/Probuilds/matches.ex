3c6ab736291786e68682cb5a62c51a2e3903d535
Niral Patel <niralpatel@Nirals-MacBook-Air.local>
2018-11-27 15:49:23 -0600

    Updating matches

--- probuilds/lib/Probuilds/matches.ex
+++ probuilds/lib/Probuilds/matches.ex
@@ -18,14 +18,32 @@ defmodule Matches do
     end
 
     # Match History Details
-    def detailsMatchHistory(matchId, accountId) do
+    # iex(14)> Matches.detailsMatchHistory(2876054290, 233564428, 1)
+    def detailsMatchHistory(matchId, accountId, partid) do
       { :ok, response } = Helperfunction.checkValidJson("https://na1.api.riotgames.com/lol/match/v3/timelines/by-match/#{matchId}/?api_key=")
-      response
-      |> Map.get("frames")
-      |> Enum.map(fn i -> skillsLevelUpDetails(i |> Map.get("events")) end)
+      response |> Map.get("frames")
+      |> Enum.map(fn i -> Enum.filter(i |> Map.get("events"), fn map -> map["type"] == "ITEM_PURCHASED" and map["participantId"] == partid end) end)
+      |> skillsandItemsDetails()
+      |> printMap()
     end
 
-    defp skillsLevelUpDetails(response) do
-      Enum.filter(response, fn map -> map["type"] == "ITEM_PURCHASED" end)
+    defp printMap(mapping) do
+      '#{mapping}mapping'
     end
+
+     defp skillsandItemsDetails(response) do
+       map = %{a: 1,}
+       Enum.each response, fn rest ->
+         if rest != [] do
+           Enum.each rest, fn res ->
+               # Map.put(map, res |> Map.get("timestamp"), 2)
+
+               time = IO.inspect res |> Map.get("timestamp")
+               itemId = IO.inspect res |> Map.get("itemId")
+               Map.put(map, time, itemId)
+           end
+         end
+         {map}
+       end
+     end
   end
